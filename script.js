dc747a4bb3bb986dfe8d348b86c18265be47cd73
nayanatara07 <nayana2017tara@gmail.com>
2023-11-28 00:36:14 +0530

    pushing comedy

--- script.js
+++ script.js
@@ -144,10 +144,37 @@ fetch(requests.fetchActionMovies)
             poster.src = img_url + movie.backdrop_path;
             row_posters.appendChild(poster);
         
-        })
-    })
+        });
+    });
 
 //comedy
+fetch(requests.fetchComedyMovies)
+    .then((res) => res.json())
 
+    .then((data) => {
+        const headrow = document.getElementById("headrow");
+        const row = document.createElement("div");
+        row.className = "row";
+        headrow.appendChild(row);
 
+        const title = document.createElement("h2");
+        title.className = "row__title";
+        title.innerText = "Comedy Movies";
+        row.appendChild(title);
+
+        const row_posters = document.createElement("div");
+        row_posters.className = "row__posters";
+        row.appendChild(row_posters);
+
+        data.results.forEach((movie) => {
+
+            const poster = document.createElement("img");
+            poster.className = "row__poster";
+            var s2 = movie.id;
+            poster.id = s2;
+            poster.src = img_url + movie.backdrop_path;
+            row_posters.appendChild(poster);
+
+        });
+    });
 
